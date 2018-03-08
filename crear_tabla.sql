b0a528a3ae72c1b62ab2d7d41150a8893f543e2e
AzulColomo <37162960+AzulColomo@users.noreply.github.com>
2018-03-07 19:50:20 -0600

    Add files via upload

---
+++ crear_tabla.sql
@@ -0,0 +1,49 @@
+--practica 1
+
+autor_ID
+nombre
+apellido
+genero
+fecha de nacimiento
+pais de origen
+
+autor_ID int
+nombre varchar (50)
+apellido varchar (50)
+genero char (2)
+fecha_d_nac date
+pais_d_origen varchar (50)
+
+create table autores(
+autor_ID int,
+nombre varchar (50),
+apellido varchar (50),
+genero char (2),
+fecha_d_nac date,
+pais_d_origen varchar (50)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ insertar.sql
@@ -0,0 +1,14 @@
+
+
+
+insert into autores(autor_ID int,nombre varchar,apellido varchar(50),genero char,fecha_d_nac,pais_d_origen)
+	values(1,'victor','frank','M','2018-03-03','MX') (2,'Manuel','Torres','M','2018-03-03','MEX'),
+	(3,'Jose','Mateos','M','2018-03-03','MEX'),
+	(4,'Antonio','Rodrigues','M','2018-03-03','MEX'),
+	(5,'Manuel','Torres','M','2018-03-03','MEX'),
+	(6,'Alexander','Reyes','M','2018-03-03','MEX'),
+	(7,'Gady','Alfonso','M','2018-03-03','MEX'),
+	(8,'Azul','Colomo','F','2018-03-03','MEX'),
+	(9,'Stefhania','Gomez','F','2018-03-03','MEX'),
+    (10,'Yadi','Reyes','F','2018-03-03','MEX'));
+
